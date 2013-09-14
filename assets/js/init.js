916a8ae5b684d353f7bf0520be39fa60b791e7eb
Carsten Sandker <csandker@googlemail.com>
2013-09-14 12:43:20 +0200

    text and style fixes

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,21 +3,4 @@
     checkedLabel: 'Online',
     uncheckedLabel: 'Offline'
   });
-
-  $(':checkbox').change(function(){
-    console.log("changed")
-    if(this.is(':checked')){
-      console.log("checked")
-    }else{
-      console.log("unchecked")
-    }
-  });
-
-  $(":checkbox").change(function() {
-    console.log("sd")
-    if(this.checked) {
-        //Do stuff
-    }
-  });
-
 });
